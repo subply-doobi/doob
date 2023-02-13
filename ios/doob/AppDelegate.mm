1bb609e18873a0a2c2ba6d51547e64294562fe32
Inhoob <inhoob@gmail.com>
2023-02-14 00:40:15 +0900

    [FEAT] : iOS 로그인 기능 추가

--- ios/doob/AppDelegate.mm
+++ ios/doob/AppDelegate.mm
@@ -1,5 +1,5 @@
 #import "AppDelegate.h"
-
+#import <RNKakaoLogins.h>
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
@@ -31,6 +31,16 @@
 - (BOOL)concurrentRootEnabled
 {
   return true;
+}
+
+- (BOOL)application:(UIApplication *)app
+     openURL:(NSURL *)url
+     options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+  if([RNKakaoLogins isKakaoTalkLoginUrl:url]) {
+    return [RNKakaoLogins handleOpenUrl: url];
+  }
+
+  return NO;
 }
 
 @end
