802ca0b646ca585e9d250725bf080b09131735f6
BRYRB <bryanopew@gmail.com>
2023-03-08 15:44:04 +0900

    [ETC] : merge 23/03/08

--- src/query/queries/product.ts
+++ src/query/queries/product.ts
@@ -1,15 +1,4 @@
-import axios from 'axios';
 import {BASE_URL, CREATE_PRODUCT_MARK, DELETE_PRODUCT_MARK} from './urls';
-import {useMutation} from '@tanstack/react-query';
-import {mutationFn} from './requestFn';
-// const getProductList = async () => {
-//   try {
-//     const res = await axios.get(PRODUCT_LIST);
-//     return res.data;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
 
 export const useCreateProductMark = () => {
   const mutation = useMutation({
